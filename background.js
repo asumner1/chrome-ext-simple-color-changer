a537fd1b0590b57f31568fd725c894eab1022f0f
asumner1 <andrew.r.sumner@gmail.com>
2024-08-26 17:36:18 -0400

    first commit

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({colorCount: 0});
+  });
+  
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.action === "colorChanged") {
+        console.log("Background color was changed");
+      }
+    }
+  );
